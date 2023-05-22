fd7631191bcc674a46d2720619ac5e5086a22af2
Anwar <anwarjahid449@gmail.com>
2023-05-22 18:13:31 +0600

    implement post method mongodb

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
 console.log(process.env.DB_USER);
 
@@ -28,13 +28,55 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    
+
+    const carCollection = client.db('kidsCarsDb').collection('cars');
+    const toyCollection = client.db('kidsCarsDb').collection('toys');
+
+    app.get('/cars',async(req,res)=>{
+        const cursor = carCollection.find();
+        const result  = await  cursor.toArray();
+        res.send(result)
+    })
+
+    app.get('/cars/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+
+        const options = {
+            // Include only the `title` and `imdb` fields in the returned document
+            projection: { name: 1, price: 1, image:1,},
+        };
+
+        const result = await carCollection.findOne(query, options);
+        res.send(result);
+    })
+
+
+    app.get('/toys', async (req, res) => {
+            console.log(req.query.email);
+            let query = {};
+            if (req.query?.email) {
+                query = { email: req.query.email }
+            }
+            const result = await toyCollection.find(query).toArray();
+            res.send(result);
+        })
+
+    app.post('/toys', async (req, res) => {
+        const booking = req.body;
+        console.log(booking);
+        const result = await toyCollection.insertOne(booking);
+        res.send(result);
+    });
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
